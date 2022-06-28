71223bd61b39a85f2ec5306b8792553f5bc7073c
saamon <suzuken2960@gmail.com>
2022-06-29 01:07:06 +0900

    ヘッダーの作成(ルーティング機能)

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,4 +1,4 @@
-import {FC, memo} from "react";
+import {FC, memo, useCallback} from "react";
 import {
     Box,
     Flex,
@@ -7,9 +7,19 @@ import {
 } from "@chakra-ui/react";
 import {MenuIconButton} from "../../atoms/button/MenuIconButton";
 import {MenuDrawer} from "../../molecules/MenuDrawer";
+import {useHistory} from "react-router-dom";
 
 export const Header: FC = memo(() => {
     const {isOpen, onOpen, onClose} = useDisclosure();
+
+    const history = useHistory();
+
+    const onClickHome = useCallback(() => history.push("/home"), []);
+    const onClickUserManagement = useCallback(
+        () => history.push("/home/user_management"),
+        []
+    );
+    const onClickSetting = useCallback(() => history.push("/home/setting"), []);
     return (
         <>
             <Flex as="nav"
@@ -19,19 +29,20 @@ export const Header: FC = memo(() => {
                   justify="space-between"
                   padding={{base: 3, md: 5}}
             >
-                <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}}>
+                <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}} onClick={onClickHome}>
                     <Heading as="h1" fontSize={{base: "md", md: "lg"}}>
                         ユーザー管理アプリ</Heading>
                 </Flex>
                 <Flex align="center" fontSize="sm" flexGrow={2} display={{base: "none", md: "flex"}}>
                     <Box pr={4}>
-                        <Link>ユーザ一覧</Link>
+                        <Link onClick={onClickUserManagement}>ユーザ一覧</Link>
                     </Box>
-                    <Link>設定</Link>
+                    <Link onClick={onClickSetting}>設定</Link>
                 </Flex>
                 <MenuIconButton onOpen={onOpen}/>
             </Flex>
-            <MenuDrawer onClose={onClose} isOpen={isOpen}/>
+            <MenuDrawer onClose={onClose} isOpen={isOpen} onClickSetting={onClickSetting} onClickHome={onClickHome}
+                        onClickUserManagement={onClickUserManagement}/>
         </>
     )
 });
