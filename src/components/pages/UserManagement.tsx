3bf3ee405c411618e41febc19fb838da21efc3ad
saamon <suzuken2960@gmail.com>
2022-07-01 01:32:14 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,32 @@
-import {FC, memo} from "react";
-import {Wrap, WrapItem} from "@chakra-ui/react";
+import {FC, memo, useEffect} from "react";
+import {Center, Spinner, Wrap, WrapItem} from "@chakra-ui/react";
 import {UserCard} from "../organisms/user/UserCard";
+import {useAllUsers} from "../../hooks/useAllUsers";
+
 
 export const UserManagement: FC = memo(() => {
+    const {getUsers, loading, users} = useAllUsers()
+
+    useEffect(() => getUsers(), [])
+
+    // @ts-ignore
     return (
-        <Wrap p={{base: 4, md: 10}}>
-            <WrapItem>
-                <UserCard imageUrl={"https://source.unsplash.com/random"} userName={"Kent"} fullName={"Kent Suzuki"}/>
-            </WrapItem>
-        </Wrap>
+        <>
+            {loading ? (
+                <Center h={"100vh"}>
+                    <Spinner/>
+                </Center>
+            ) : (
+                <Wrap p={{base: 4, md: 10}}>
+                    {users.map((user) => (
+                        <WrapItem key={user.id} mx={"auto"}>
+                            <UserCard imageUrl={"https://source.unsplash.com/random"} userName={user.username}
+                                      fullName={user.name}
+                            />
+                        </WrapItem>
+                    ))}
+                </Wrap>
+            )}
+        </>
     )
 });
