695f7b53e4207746e2a2de8f8cc0f73f07e39339
saamon <suzuken2960@gmail.com>
2022-06-30 22:51:58 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,13 @@
 import {FC, memo} from "react";
+import {Wrap, WrapItem} from "@chakra-ui/react";
+import {UserCard} from "../organisms/user/UserCard";
 
-export const UserManagement:FC=memo(()=>{
-    return<p>ユーザー管理ページです</p>
+export const UserManagement: FC = memo(() => {
+    return (
+        <Wrap p={{base: 4, md: 10}}>
+            <WrapItem>
+                <UserCard imageUrl={"https://source.unsplash.com/random"} userName={"Kent"} fullName={"Kent Suzuki"}/>
+            </WrapItem>
+        </Wrap>
+    )
 });
