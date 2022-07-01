71c66317ff487422c1a0c4c2a522369ed5db9ebb
saamon <suzuken2960@gmail.com>
2022-07-01 22:40:07 +0900

    ユーザー詳細モーダル画面の作成

--- src/components/organisms/user/UserCard.tsx
+++ src/components/organisms/user/UserCard.tsx
@@ -5,14 +5,17 @@ type Props = {
     imageUrl: string;
     userName: string;
     fullName: string;
+    onClick: () => void;
 };
 
 export const UserCard: FC<Props> = props => {
-    const {imageUrl, userName, fullName} = props;
+    const {imageUrl, userName, fullName, onClick} = props;
 
     return (
         <Box w={"260px"} h={"260px"} bg={"white"} borderRadius={"10px"} shadow={"md"} p={4}
-             _hover={{cursor: "pointer", opacity: 0.8}}>
+             _hover={{cursor: "pointer", opacity: 0.8}}
+             onClick={onClick}
+        >
             <Stack textAlign={"center"}>
                 <Image borderRadius={"full"} boxSize={"160px"}
                        src={imageUrl}
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,23 @@
-import {FC, memo, useEffect} from "react";
-import {Center, Spinner, Wrap, WrapItem} from "@chakra-ui/react";
+import {FC, memo, useCallback, useEffect} from "react";
+import {
+    Center,
+    Spinner,
+    useDisclosure,
+    Wrap,
+    WrapItem
+} from "@chakra-ui/react";
 import {UserCard} from "../organisms/user/UserCard";
 import {useAllUsers} from "../../hooks/useAllUsers";
+import {UserDetailModal} from "../organisms/user/UserDetailModal";
 
 
 export const UserManagement: FC = memo(() => {
+    const {isOpen, onOpen, onClose} = useDisclosure();
     const {getUsers, loading, users} = useAllUsers()
 
     useEffect(() => getUsers(), [])
+
+    const onClickUser = useCallback(() => onOpen(), []);
 
     // @ts-ignore
     return (
@@ -21,12 +31,13 @@ export const UserManagement: FC = memo(() => {
                     {users.map((user) => (
                         <WrapItem key={user.id} mx={"auto"}>
                             <UserCard imageUrl={"https://source.unsplash.com/random"} userName={user.username}
-                                      fullName={user.name}
+                                      fullName={user.name} onClick={onClickUser}
                             />
                         </WrapItem>
                     ))}
                 </Wrap>
             )}
+            <UserDetailModal isOpen={isOpen} onClose={onClose}/>
         </>
     )
 });
