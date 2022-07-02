a818052aff1a3fcf2d229acca01105825f2af968
saamon <suzuken2960@gmail.com>
2022-07-02 14:54:33 +0900

    ログインユーザー情報をcontextに保持

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,34 +1,37 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { HomeRoutes } from "./HomeRoutes";
+import {Route, Switch} from "react-router-dom";
+import {HomeRoutes} from "./HomeRoutes";
 import {Login} from "../components/pages/Login";
-import { Page404 } from "../components/pages/Page404";
+import {Page404} from "../components/pages/Page404";
 import {HeaderLayout} from "../components/templates/HeaderLayout";
+import {LoginUserProvider} from "../providers/LoginUserProvider";
 
 export const Router = () => {
     return (
         <Switch>
-            <Route exact path="/">
-                <Login/>
-            </Route>
-            <Route
-                path="/home"
-                render={({ match: { url } }) => (
-                    <Switch>
-                        {HomeRoutes.map(route => (
-                            <Route
-                                key={route.path}
-                                exact={route.exact}
-                                path={`${url}${route.path}`}
-                            >
-                                <HeaderLayout>{route.children}</HeaderLayout>
-                            </Route>
-                        ))}
-                    </Switch>
-                )}
-            />
+            <LoginUserProvider>
+                <Route exact path="/">
+                    <Login/>
+                </Route>
+                <Route
+                    path="/home"
+                    render={({match: {url}}) => (
+                        <Switch>
+                            {HomeRoutes.map(route => (
+                                <Route
+                                    key={route.path}
+                                    exact={route.exact}
+                                    path={`${url}${route.path}`}
+                                >
+                                    <HeaderLayout>{route.children}</HeaderLayout>
+                                </Route>
+                            ))}
+                        </Switch>
+                    )}
+                />
+            </LoginUserProvider>
             <Route path="*">
-                <Page404 />
+                <Page404/>
             </Route>
         </Switch>
     );
